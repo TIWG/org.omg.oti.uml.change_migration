25b56d2170476de6cffbd998af6682dd9d230546
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-01-27 00:12:52 +0000

    - SVN revision 445380

---
+++ project/Versions.scala
@@ -0,0 +1,14 @@
+
+object Versions {
+
+  // this project
+  val version = "445380"
+
+  // https://github.jpl.nasa.gov/imce/org.omg.oti
+  val oti_core="445379"
+
+  // Eclipse EMF (emf.ecore, emf.ecore.xmi, emf.common)
+  // Note must be binary compatible with the EMF used in OTI adapters (MD, Papyrus)
+  // otherwise there would have to be adapter-specific versions of this package.
+  val emf_ecore = "2.10.1"
+}
