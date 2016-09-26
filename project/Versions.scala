2004def4222ba419d2a1fd45b8fd81d79bde9a0c
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2016-09-26 04:12:21 +0000

    - Delete obsolete svn folder - Cleanup classpath - pom git tags

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
 
   // this project
-  val version = "3.0.1"
+  val version = "3.0.2"
 
   // Eclipse EMF (emf.ecore, emf.ecore.xmi, emf.common)
   // Note must be binary compatible with the EMF used in OTI adapters (MD, Papyrus)
